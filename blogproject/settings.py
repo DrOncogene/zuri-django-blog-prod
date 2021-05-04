a41177de5e110f9d87c37485c8c2dd0516d99d5a
Rasheed Nasir <mypythtesting@gmail.com>
2021-05-04 23:57:02 +0100

    made the repo ready for deployment on heroku

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -133,3 +133,8 @@ LOGIN_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
