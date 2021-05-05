7aa6322d2dd69312318d3d3b61783a9ec28c652d
Rasheed Nasir <mypythtesting@gmail.com>
2021-05-05 01:34:38 +0100

    minor changes

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myzuri-django-blog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
